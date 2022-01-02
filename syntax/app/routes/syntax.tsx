03a654b8ec35130fa461cfbfb16938aa3ef4a930
ck9891 <cameronfarquharson1@gmail.com>
2022-01-02 14:27:37 -0500

    2 files modified, 1 added

--- syntax/app/routes/syntax.tsx
+++ syntax/app/routes/syntax.tsx
@@ -1,3 +1,19 @@
+import { Link, Outlet } from 'remix';
+
 export default function () {
-  return <h1>The Syntax Podcast</h1>;
+  return (
+    <div>
+      <section>
+        <h1>The Syntax Podcast</h1>
+        <aside>
+          <nav>
+            <ul>
+              <Link to="/syntax/show-1">Episode 1</Link>
+            </ul>
+          </nav>
+        </aside>
+      </section>
+      <Outlet />
+    </div>
+  );
 }
---
+++ syntax/app/routes/syntax/show-1.tsx
@@ -0,0 +1,3 @@
+export default function () {
+  return <h1>Episode 1</h1>;
+}
